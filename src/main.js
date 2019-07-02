ba37db6a6f29ded15fe7d3d617b6cf84d92cd8f0
trinitysilvia <trinitysilviag@gmail.com>
2019-07-02 17:25:01 -0500

    codeando funcion con foreach

--- src/main.js
+++ src/main.js
@@ -3,9 +3,34 @@
 //llamando el contenido de la DATA "rickandmorty.js"
 const data = window.RICKANDMORTY.results;
 
-data.forEach(imprimirData);
-
-function imprimirData(item, index) {
-    
-    document.getElementById("info-data").innerHTML += index+ ' ' + item + "<br>"; 
+data.forEach(Element
   }
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  /*
+  const data = window.RICKANDMORTY.results;
+  data.forEach(imprimirData);
+
+  function imprimirData(element) {
+  document.getElementById('info-data').innerHTML += element.name + "<br>" + element.location.name +"<br>";
+  }
+  
+  
+  
+  
+  
+  
+  const data = window.RICKANDMORTY.results
+data.forEach(imprimirData);
+function imprimirData(item, index) {
+document.getElementById("info-data").innerHTML += index+ ' ' + item + "<br>";
+document.write(index + element.name+"<br>"); 
+} */
