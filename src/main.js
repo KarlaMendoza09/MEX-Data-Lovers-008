7199c72a2d390cf0c6d8df672e6715185805879b
trinitysilvia <trinitysilviag@gmail.com>
2019-07-04 00:13:42 -0500

    desarrollando funcion filtrado

--- src/main.js
+++ src/main.js
@@ -1,43 +1,39 @@
 /*FUNCION MOSTRAR DATA EN HTML */
 
-//llamando el contenido de la DATA "rickandmorty.js"
-const data = window.RICKANDMORTY.results;
+const data = window.RICKANDMORTY.results;     //llamando el contenido de la DATA "rickandmorty.js"
+const infoData = document.getElementById('info-data');     //llamando el div donde vamos a imprimir la data
 
-//llamando el div donde vamos a imprimir la data
-const infoData = document.getElementById('info-data');
+let string = '';     // creando variable que contendra el string
 
-
-/*HACIENDO L FUNCION PARA IMPRIMIR CONTENIDO EN HTML */
-
-let string = '';   // creando variable que contendra el string
-
-data.forEach((element, index) => {         // DESARROLLLO DE FUNCION
-  // console.log(`${index} 
-  // IMAGE: ${element.image} 
-  // NOMBRE: ${element.name} 
-  // DIMENSION: ${element.origin.name}`
-  // console.log(element.image)
-
-  string += `<div class="contenido">
- <div class= "tarjeta">
-    <div class="contenido-tarjeta"> 
+data.forEach((element, index) => { // desarrollo de la funcion
+  string += `<div class="tarjeta">     
+ <div class= "retrato">
+    <div class="texto"> 
+    <h3>${element.name} </h3>
     <img src="${element.image}" alt="">
-      <h3>${element.name} </h3>
-        <p>${element.origin.name} </p>
+    
      </div>
   </div>
 </div>`
-})
-
-
+});
 //console.log(string);
 infoData.innerHTML = string;
+
+/* FILTRAR INFO */
+//hacer boton para que aparesca el filtrado
+const btnFiltro= document.getElementById('btn-filtro');
+
+const primer = data.filter(dats => (dats.name === 'Abradolf Lincler'));
+console.log(primer);
+
+/*infoData.innerHTML=btnFiltro.addEventListener ('click', primer); */
   
 
 
-
-  /*
+/*
 ----------------------------------------------------------------
+
+<p>${element.location.name} </p>
 
 data.forEach((element, index) => 
 console.log(`${index} 
