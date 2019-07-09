c5d1f766338e44ed6852fa2d046c6c7a6349ee04
trinitysilvia <trinitysilviag@gmail.com>
2019-07-09 15:11:43 -0500

    llamando eventos interactivos

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 
 const data = window.RICKANDMORTY.results;  //llamando el contenido de la DATA "rickandmorty.js"   
-const infoData = document.getElementById('info-data');  //llamando el div donde vamos a imprimir la data   
+const viewData = document.getElementById('view-data');  //llamando el div donde vamos a imprimir la data   
 
-
-
-const btnAllData= document.getElementById('btn-allData');  //BOTON MOSTRAR TODOS LOS PERSONAJES
-
+/*
 let stringAllData = '';     // creando variable que guardara toda la data (string)
 const allData = data.forEach((element, index) => {
   stringAllData += `<div class="tarjeta"> 
@@ -17,15 +14,40 @@ const allData = data.forEach((element, index) => {
 </div>`
 console.log(stringAllData);
 });
-btnAllData.addEventListener('click', () => { infoData.innerHTML= stringAllData; })
+btnAllData.addEventListener('click', () => { infoData.innerHTML= stringAllData; })  */
 
 /* ---------- ordenando de a la z y viceversa ---------- */
 
-document.getElementById('dropDownSelecter').addEventListener('click', () => {
-  const indexSort= document.getElementById('dropDownSelecter');
+const sortData = (selectedOrder, data) => {
+  if (SelectedOrder === 1) {
+    return data.sort(function(elementA, elementB) {
+      if(elementA.name > elementB.name){
+        return 1;
+      }
+      if (elementA.name < elementB.name) {
+        return -1;
+      }
+      return 0;
+    });
 
-  const selectedSort= indexSort[indexSort.selectedIndex].value;
+  } else if (selectedOrder === 2) {
+    return data.sort(function(elementA, elementB) {
+      if(elementA.name < elementB.name) {
+        return 1;
+      }
+      if(elementA.name > elementB.name) {
+        return 0;
+      }
+    })
+  }
+};
+
+
+document.getElementById('interactiveSelecter').addEventListener('click', () => {
+  const indexOrder= document.getElementById('interactiveSelecter'); //obtenemos text
+
+  const selectedOrder= indexOrder[indexOrder.selectedIndex].value; // obtenemos valor
   
-  const arraySort= window.RICKANDMORTY.results.orderBy(selectedSort, data);
-  infoData.innerHTML= arraySort;
+  const newArrayOrder= sortData(selectedOrder, data);
+  infoData.innerHTML= data;
 });
