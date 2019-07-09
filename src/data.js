92c608aafd87545c47bfd158687a542c2dd72e1a
trinitysilvia <trinitysilviag@gmail.com>
2019-07-09 14:10:20 -0500

    rompiendo codigo para depurar funciones

--- src/data.js
+++ src/data.js
@@ -1,8 +1,36 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
+/*const example = () => {
   return 'example';
 };
 
-window.example = example;
+window.example = example; */
+window.RICKANDMORTY.results = {
+// FUNCION QUE ORDENA
+orderBy: (selectedSort, data) => {
+  if (SelectedSort === 2) {
+    return data.sort(function(prev, next) {
+      if(prev.name > next.name){
+        return 1;
+      }
+      if (prev.name < next.name) {
+        return -1;
+      }
+      return 0;
+    });
+
+  } else if (selectedSort === 3) {
+    return data.sort(function(prev, netx) {
+      if(prev.name < netx.name) {
+        return 1;
+      }
+      if(prev.name > next.name) {
+        return 0;
+      }
+    })
+  }
+}
+
+};
+/// limpiando codigo...
--- src/main.js
+++ src/main.js
@@ -15,61 +15,17 @@ const allData = data.forEach((element, index) => {
     <p>ESPECIE: ${element.species}</p>
     <p>GÉNERO: ${element.gender}</p>
 </div>`
-//console.log(stringAllData);
+console.log(stringAllData);
 });
-btnAllData.addEventListener('click', ()=> {infoData.innerHTML= stringAllData })
+btnAllData.addEventListener('click', () => { infoData.innerHTML= stringAllData; })
 
+/* ---------- ordenando de a la z y viceversa ---------- */
 
-const btnEarth= document.getElementById('btn-earth');  //BOTON MOSTRAR DIMENSION EARTH C137
+document.getElementById('dropDownSelecter').addEventListener('click', () => {
+  const indexSort= document.getElementById('dropDownSelecter');
 
-let stringAllEarthC = '';
-const sistems = data.filter((planets) => { 
-  if (planets.origin.name === 'Earth (C-137)') {
-    stringAllEarthC += `<div class="tarjeta"> 
-    <img src="${planets.image}" alt="">
-    <h3>${planets.name} </h3>
-    <p>ESPECIE: ${planets.species}</p>
-    <p>GÉNERO: ${planets.gender}</p>
-</div>`
-   //console.log(stringAllPlanets);
-  }
+  const selectedSort= indexSort[indexSort.selectedIndex].value;
+  
+  const arraySort= window.RICKANDMORTY.results.orderBy(selectedSort, data);
+  infoData.innerHTML= arraySort;
 });
-btnEarth.addEventListener('click', () => {infoData.innerHTML = stringAllEarthC})
-
-
-
-
-const btnRicks= document.getElementById('btn-ricks');  //BOTON MOSTRAR RICKS
-let stringAllRicks = '';
-
-const personRk = data.filter((rickSan) => {  
-  if (rickSan.name === 'Rick Sanchez') {
-    stringAllRicks += `<div class="tarjeta"> 
-    <img src="${rickSan.image}" alt="">
-    <h3>${rickSan.name} </h3>
-    <p>DIMENSIÓN: ${rickSan.origin.name}</p>
-    <p>ESPECIE: ${rickSan.species}</p>
-</div>`
-   //console.log(stringAllRicks);
-  }
-});
-btnRicks.addEventListener('click', () => {infoData.innerHTML = stringAllRicks })
-
-
-
-
-const btnMortys= document.getElementById('btn-mortys');   //BOTON MOSTRAR MORTYS
-let stringAllMortys = '';
-
-const personMt = data.filter((mortySm) => { 
-  if (mortySm.name === 'Morty Smith') {
-    stringAllMortys += `<div class="tarjeta"> 
-    <img src="${mortySm.image}" alt="">
-    <h3>${mortySm.name} </h3>
-    <p>DIMENSIÓN: ${mortySm.origin.name}</p>
-    <p>ESPECIE: ${mortySm.species}</p>
-</div>`
-   //console.log(stringAllMortys);
-  }
-});
-btnMortys.addEventListener('click',() => {infoData.innerHTML = stringAllMortys })
